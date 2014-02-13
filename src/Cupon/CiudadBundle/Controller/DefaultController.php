8b2a2ba835b7af0becb590cfe1b9b2c76af366a3
Rafael Minguet García <rminguet@gmail.com>
2014-02-13 14:40:08 +0100

    Now, user can select the city to see.

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -3,11 +3,34 @@
 namespace Cupon\CiudadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	/**
+	* Cambia a una ciudad determinada (cuando se selecciona desde la lista de ciudades).
+	* @param string $ciudad La ciudad a la que se debe redirigir.
+	*/
+    public function cambiarAction($ciudad)
     {
-        return $this->render('CiudadBundle:Default:index.html.twig', array('name' => $name));
+        return new RedirectResponse(
+            $this->generateUrl( 'portada', array('ciudad' => $ciudad))
+        );
+    }
+
+    /**
+    * Devuelve el listado de todas las ciudades.
+    * @param string Slug de la ciudad que está actualmente seleccionada.
+    * @return La lista de ciudades de la bbdd y la que actualmente está seleccionada.
+    */
+    public function listaCiudadesAction($ciudad)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ciudades = $em->getRepository('CiudadBundle:Ciudad')->findAll();
+
+        return $this->render(
+            'CiudadBundle:Default:listaCiudades.html.twig', 
+            array('ciudades' => $ciudades, 'ciudadActual' => $ciudad)
+        );
     }
 }
