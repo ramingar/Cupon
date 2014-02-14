241d4161151f874d17f52c533e69db4a26a2b0df
Rafael Minguet García <rminguet@gmail.com>
2014-02-14 14:25:47 +0100

    Offer details added. Factorizing common code in twig templates. Related offers in offer detail page.

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -6,6 +6,10 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
+    /**
+    * Muestra la portada que corresponde a la ciudad que se le pasa por parámetro.
+    * @param string $ciudad El slug de la ciudad de la que se quiere ver la portada.
+    */
     public function portadaAction($ciudad = null)
     {
         /* El siguiente código ya no es necesario.                                              */
@@ -34,6 +38,23 @@ class DefaultController extends Controller
         return $this->render(
             'OfertaBundle:Default:portada.html.twig',
             array('oferta' => $oferta)
+        );
+    }
+
+    /**
+    * Muestra la página del detalle para la oferta y ciudad que se le pasa por parámetro.
+    * @param string $ciudad El slug de la ciudad donde está la oferta.
+    * @param string $slug El slug de la oferta que se desea ver en detalle.
+    */
+    public function ofertaAction($ciudad, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOferta($ciudad, $slug);
+        $relacionadas = $em->getRepository('OfertaBundle:Oferta')->findRelacionadas($ciudad);
+
+        return $this->render(
+            'OfertaBundle:Default:detalle.html.twig', 
+            array('oferta' => $oferta, 'relacionadas' => $relacionadas)
         );
     }
 }
