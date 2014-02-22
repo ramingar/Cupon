26f3bf90bde7f18dab8e17b7dfe3ec9c09b0a47a
Rafael Minguet García <rminguet@gmail.com>
2014-02-22 19:41:49 +0100

    Shop's main page.

--- src/Cupon/TiendaBundle/Controller/DefaultController.php
+++ src/Cupon/TiendaBundle/Controller/DefaultController.php
@@ -6,8 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	/**
+	 * A partir de la ciudad y del nombre de la tienda, muestra la
+	 * página de esa tienda.
+	 * @param string $ciudad El slug de la ciudad en la que se encuentra.
+	 * @param string $tienda El slug de la tienda.
+	 */
+    public function portadaAction($ciudad, $tienda)
     {
-        return $this->render('TiendaBundle:Default:index.html.twig', array('name' => $name));
-    }
+        $em = $this->getDoctrine()->getManager();
+        $datosCiudad   = $em->getRepository('CiudadBundle:Ciudad')->findOneBySlug($ciudad);
+        $datosTienda   = $em->getRepository('TiendaBundle:Tienda')->findOneBy(
+                             array('ciudad' => $datosCiudad->getId(), 'slug' => $tienda)
+                         );
+        
+        if (!$datosTienda){
+            throw $this->createNotFoundException('No existe esta tienda');
+        }
+
+        $ofertasTienda = $em->getRepository('TiendaBundle:Tienda')
+                            ->findUltimasOfertasPublicadas($datosTienda->getId());
+
+        $cercanas = $em->getRepository('TiendaBundle:Tienda')->findCercanas(
+                        $datosTienda->getSlug(),
+                        $datosTienda->getCiudad()->getSlug(),
+                        5
+                    );
+
+        return $this->render('TiendaBundle:Default:portada.html.twig', array(
+                             'tienda'   => $datosTienda,
+                             'ofertas'  => $ofertasTienda,
+                             'cercanas' => $cercanas
+                      ));
+    } 
 }
