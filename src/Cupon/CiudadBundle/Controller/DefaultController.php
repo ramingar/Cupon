8e49ed3b0046e9a242f4e0e5efc48df0b0a614f1
Rafael Minguet García <rminguet@gmail.com>
2014-07-03 10:43:30 +0200

    - Security - ROLES, PROVIDERS.  - Security - Login form created.  - Security - Remember me.  - Security - Redirections.

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -44,6 +44,11 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $ciudad    = $em->getRepository('CiudadBundle:Ciudad')->findOneBySlug($ciudad);
+
+        if (!$ciudad) {
+            throw $this->createNotFoundException('No existe la ciudad');
+        }
+
         $cercanas  = $em->getRepository('CiudadBundle:Ciudad')->findCercanas ($ciudad->getId(), 5);
         $recientes = $em->getRepository('OfertaBundle:Oferta')->findRecientes($ciudad->getId(), 5);
         
