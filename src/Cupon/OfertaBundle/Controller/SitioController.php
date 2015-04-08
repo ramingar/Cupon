a8395e3ac4e0028c99c6b5872120a1e1cf5ec9db
Rafael Minguet García <rminguet@gmail.com>
2015-04-09 01:02:49 +0200

    Static pages

---
+++ src/Cupon/OfertaBundle/Controller/SitioController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cupon\OfertaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class SitioController extends Controller
+{
+    public function estaticaAction ($pagina)
+    {
+        return $this->render('OfertaBundle:Sitio:' . $pagina . '.html.twig');
+    }
+    
+}
