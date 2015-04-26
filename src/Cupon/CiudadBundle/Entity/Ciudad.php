3be1e42756df4f3bbdc7fb46d7a44e8e02110d6a
Rafael Minguet García <rminguet@gmail.com>
2015-04-26 13:10:35 +0200

    Creación del slug de la ciudad cuando le asignas un nombre.

--- src/Cupon/CiudadBundle/Entity/Ciudad.php
+++ src/Cupon/CiudadBundle/Entity/Ciudad.php
@@ -2,6 +2,7 @@
 namespace Cupon\CiudadBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Cupon\OfertaBundle\Util\Util;
 
 /**
  * @ORM\Entity
@@ -27,6 +28,7 @@ class Ciudad
     
     public function setNombre($nombre)
     {
+        $this->setSlug(Util::getSlug($nombre));
         $this->nombre = $nombre;
         return $this;
     }
