1ae57d7aa21c957d136daff9b801c93b86996ee3
Rafael Minguet García <rminguet@gmail.com>
2015-04-26 13:16:57 +0200

    Asignación del slug de Tienda cuando se le asigna el nombre.

--- src/Cupon/TiendaBundle/Entity/Tienda.php
+++ src/Cupon/TiendaBundle/Entity/Tienda.php
@@ -2,6 +2,7 @@
 namespace Cupon\TiendaBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Cupon\OfertaBundle\Util\Util;
 
 /**
  * @ORM\Entity
@@ -58,8 +59,8 @@ class Tienda
      */
     public function setNombre($nombre)
     {
+        $this->setSlug(Util::getSlug($nombre));
         $this->nombre = $nombre;
-    
         return $this;
     }
 
