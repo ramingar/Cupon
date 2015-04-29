28d62415ac56c46eb8dc10399313f73595104ada
Rafael Minguet García <rminguet@gmail.com>
2015-04-29 22:25:09 +0200

    Lógica de la portada

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -3,14 +3,21 @@
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function ayudaAction()
+    public function portadaAction()
     {
-//         return new Response('<h1>Ayuda</h1>');
-        return $this->render('OfertaBundle:Default:ayuda.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
+            'ciudad' => 1,
+            'fechaPublicacion' => new \DateTime('today - 1 sec')
+        ));
+        
+        return $this->render(
+            'OfertaBundle:Default:portada.html.twig',
+            array('oferta'=>$oferta)
+        );
     }
     
 }
