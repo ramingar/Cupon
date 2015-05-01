c365b3496c0c4057503dd53603658fe320e99c0b
Rafael Minguet García <rminguet@gmail.com>
2015-05-01 22:01:57 +0200

    Refactorizando el controlador de la portada. Añadiendo redirección cuando el usuario accede a '/' en lugar de a '/{ciudad}'.

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -6,11 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function portadaAction()
+    public function portadaAction($ciudad = null)
     {
+        
+        /*
+         * Si la ciudad no es pasada como parámetro,
+         * este código redirige a la portada con la ciudad por defecto
+         * asignada como parámetro.
+         * (Comentado porque la redirección se ejecuta desde el enrutado). 
+
+        if (null == $ciudad) {
+            $ciudad = $this->container
+            ->getParameter('cupon.ciudad_por_defecto');
+        
+            return new RedirectResponse(
+                $this->generateUrl('portada', array('ciudad' => $ciudad))
+            );
+        }
+        */
+        
         $em = $this->getDoctrine()->getManager();
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
-            'ciudad' => $this->container->getParameter('cupon.ciudad_por_defecto'),
+            'ciudad' => $ciudad,
             'fechaPublicacion' => new \DateTime('today - 1 sec')
         ));
         
