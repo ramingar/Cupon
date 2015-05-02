d4f56c7effc61286a98e8ed9d8004d934caddee0
Rafael Minguet García <rminguet@gmail.com>
2015-05-02 20:31:55 +0200

    Repositorio propio para la entidad Oferta y método para recoger la oferta activa del día.

--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -7,5 +7,24 @@ class OfertaRepository extends EntityRepository
 {
     public function findOfertaDelDia($ciudad)
     {
+        $em = $this->getEntityManager();
+        $consulta = $em->createQuery(
+            'SELECT o, c, t 
+            FROM OfertaBundle:Oferta o 
+            JOIN o.ciudad c 
+            JOIN o.tienda t
+            WHERE c.slug = :ciudad 
+            AND o.fechaPublicacion = :fechaPublicacion 
+            AND o.revisada = true 
+            ORDER BY o.fechaPublicacion DESC'
+        );
+        $consulta->setParameters(array(
+            'ciudad' => $ciudad,
+            'fechaPublicacion' => new \DateTime('today - 1 sec')
+        ));
+        $consulta->setMaxResults(1);
+        
+        return $consulta->getSingleResult();
+        
     }
 }
